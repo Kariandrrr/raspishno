fb8c694e87abdcbe68816a37a8ca9d236693d184
Kariandrrr <educationkarina08@gmail.com>
2026-06-08 20:25:47 +0300

    init enum shift

--- backend/app/enums/__init__.py
+++ backend/app/enums/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "RoomType",
     "SlotNumber",
     "DayOfWeek",
+    "Shift",
 )
 
 from .room_type import RoomType
 from .time_slots import SlotNumber
 from .days_of_week import DayOfWeek
+from .shift import Shift
