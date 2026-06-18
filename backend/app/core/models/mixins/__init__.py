ba57e0f0ee478105e7f58dff567351a40e5797ff
Kariandrrr <educationkarina08@gmail.com>
2026-06-18 15:09:07 +0300

    init

--- backend/app/core/models/mixins/__init__.py
+++ backend/app/core/models/mixins/__init__.py
@@ -1,6 +1,13 @@
 __all__ = (
     "UUIDPKMixin",
     "TimestampMixin",
+    "CreatedByMixin",
+    "UpdatedByMixin",
+    "TimestampMixin",
+    "UpdateAtMixin",
 )
 from .uuid_pk import UUIDPKMixin
 from .timestamp import TimestampMixin
+from .created_by import CreatedByMixin
+from .updated_by import UpdatedByMixin
+from .update_timestamp import UpdateAtMixin
